680e3cf836c0bdee9e652e7c52a196f6ca85361e
Arina N <arin.novikova@gmail.com>
2019-07-20 18:41:44 +0900

    Изменена функция выбора группы для поиска по укзанному индексу (4.4)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
     if(! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("for modify", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Name gr", "text header", "text footer"));
     app.getGroupHelper().submitGroupModification();
