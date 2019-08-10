94c797f21664767ab2aa5d5945602f0aeaa74874
Arina N <arin.novikova@gmail.com>
2019-08-10 20:05:12 +0900

    Добавлено получение из БД данных о контактах с условием (7.3)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,58 +3,97 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name = "middlename")
   private String mname;
   @Expose
+  @Column(name = "lastname")
   private String lastname;
   @Expose
+  @Column(name = "nickname")
   private String nickname;
   @Expose
+  @Column(name = "title")
   private String title;
   @Expose
+  @Column(name = "address")
+  @Type(type = "text")
   private String address;
   @Expose
+  @Column(name = "firstname")
   private String fname;
   @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String homeNumber;
   @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobileNumber;
   @Expose
+  @Column(name = "work")
+  @Type(type = "text")
   private String workNumber;
   @Expose
+  @Column(name = "email")
+  @Type(type = "text")
   private String email;
   @Expose
+  @Column(name = "email2")
+  @Type(type = "text")
   private String email1;
   @Expose
+  @Column(name = "email3")
+  @Type(type = "text")
   private String email2;
   @Expose
+  @Transient
   private String bDay;
   @Expose
+  @Transient
   private String bMonth;
   @Expose
+  @Transient
   private String bYear;
   @Expose
+  @Transient
   private String group;
   @Expose
+  @Column(name = "address2")
+  @Type(type = "text")
   private String secondaryAddress;
   @Expose
+  @Column(name = "phone2")
+  @Type(type = "text")
   private String secondaryHome;
   @Expose
+  @Column(name = "notes")
+  @Type(type = "text")
   private String secondaryNotes;
   @Expose
+  @Transient
   private String allPhones;
   @Expose
+  @Transient
   private String allEmails;
   @Expose
-  private File photo;
+  @Column(name="photo")
+  @Type(type = "text")
+  private String photo;
 
   public String getMname() {
     return mname;
@@ -145,7 +184,7 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withId(int id) {
@@ -259,7 +298,7 @@ public class ContactData {
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
