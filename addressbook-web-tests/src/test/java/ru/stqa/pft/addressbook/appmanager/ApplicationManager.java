afd98bec974ad27ffaef2eaaca0e2d8d08c0cdee
Arina N <arin.novikova@gmail.com>
2019-10-11 03:52:15 +0900

    настройки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -35,7 +35,7 @@ public class ApplicationManager {
   public void init() throws IOException {
     //String target = System.getProperty("target", "local");
     //properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties"))));
+    //properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties"))));
     dbHelper = new DbHelper();
 
     if("".equals(properties.getProperty("selenium.server"))) {
